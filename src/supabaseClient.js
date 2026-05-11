73cd457c337bd5da0952802ba05e24f180f11972
promcareservice-dotcom <promcareservice@gmail.com>
2026-05-11 21:07:42 +0700

    Hard Fix: Correcting syntax for Supabase client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// ต้องเขียนให้ครบตามนี้ ห้ามลบ const หรือเครื่องหมายเท่ากับออกนะครับ
 const supabaseUrl = 'https://iuhywblfqodtrabeohln.supabase.co'
 const supabaseAnonKey = 'sb_publishable_Kgt2KMpgBEc6qwEeOOh8Ig_L2-skiJR'
 
