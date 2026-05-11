c0784c037aeb732c331bc08a1623444154b224a3
promcareservice-dotcom <promcareservice@gmail.com>
2026-05-11 21:26:32 +0700

    Fix: Correcting syntax with const and equal sign

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
+// ต้องมีคำว่า const และเครื่องหมาย = ครบถ้วนตามนี้ครับ
 const supabaseUrl = 'https://iuhywblfqodtrabeohln.supabase.co'
 const supabaseAnonKey = 'sb_publishable_Kgt2KMpgBEc6qwEeOOh8Ig_L2-skiJR'
 
